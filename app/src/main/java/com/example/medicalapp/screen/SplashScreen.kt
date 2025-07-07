40dab4c0ec25112bf1e2c20cbe52dfb2bc89661e
Suman Mula <sumanmula2000@gmail.com>
2025-07-07 07:25:49 +0530

    Create SplashScreen.kt
    Create a simple screen with the logo of the app .

---
+++ app/src/main/java/com/example/medicalapp/screen/SplashScreen.kt
@@ -0,0 +1,35 @@
+package com.example.medicalapp.screen
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.medicalapp.R
+
+@Composable
+fun SplashScreen() {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFEBB9)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(R.drawable.mediquick_splash_logo),
+            contentDescription = "Splash Logo",
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreenPreview() {
+    SplashScreen()
+}
